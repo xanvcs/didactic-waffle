568377666f7d3de20a920206f9fa00d46b50c4a7
xian <shanema51@gmail.com>
2024-05-07 03:19:03 -0600

    react card component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,16 @@
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Food from './Food.jsx'
+import Card from './Card.jsx'
 
 function App() {
   return(
     <>
       <Header/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
       <Food/>
       <Food/>
       <Footer/>
---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card() {
+    return (
+        <div className="card">
+            <img className="card-image" src="https://via.placeholder.com/150" alt="profile picture"></img>
+            <h2 className="card-title">xian ma</h2>
+            <p className="card-text">i study at boise state and play videogames</p>
+        </div>
+    );
+}
+
+export default Card
